5e0c08a7371dacd0c906816211539a1cbabc8d53
Yet Siaw Yeung <Yet.Siaw.Yeung@gmail.com>
2020-12-02 19:37:57 +1100

    Reorganise input files and source

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
 fn main() {
-    part2()
+    day1::part2()
 }
-fn part1() {
-    let input: Vec<u32> = include_str!("input.txt")
-        .lines()
-        .map(|line| line.parse().unwrap())
-        .collect();
-    for (i, first) in input.iter().enumerate() {
-        for second in input[i..].iter() {
-            if first + second == 2020 {
-                println!("{}", first * second);
+
+mod day1 {
+    pub fn part1() {
+        let input: Vec<u32> = include_str!("../inputs/day1.txt")
+            .lines()
+            .map(|line| line.parse().unwrap())
+            .collect();
+        for (i, first) in input.iter().enumerate() {
+            for second in input[i..].iter() {
+                if first + second == 2020 {
+                    println!("{}", first * second);
+                }
             }
         }
     }
-}
-fn part2() {
-    let input: Vec<u32> = include_str!("input.txt")
-        .lines()
-        .map(|line| line.parse().unwrap())
-        .collect();
-    for (i, first) in input.iter().enumerate() {
-        for (j, second) in input[i..].iter().enumerate() {
-            for third in input[j..].iter() {
-                if first + second + third == 2020 {
-                    println!("{}", first * second * third);
+    pub fn part2() {
+        let input: Vec<u32> = include_str!("../inputs/day1.txt")
+            .lines()
+            .map(|line| line.parse().unwrap())
+            .collect();
+        for (i, first) in input.iter().enumerate() {
+            for (j, second) in input[i..].iter().enumerate() {
+                for third in input[j..].iter() {
+                    if first + second + third == 2020 {
+                        println!("{}", first * second * third);
+                    }
                 }
             }
         }
