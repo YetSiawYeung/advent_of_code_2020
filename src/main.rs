b017a7dd65d6a210ead05b4408d88a1941109f73
Yet Siaw Yeung <Yet.Siaw.Yeung@gmail.com>
2020-12-02 20:00:19 +1100

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    day1::part2()
+    day2::part2()
 }
 
 mod day1 {
@@ -30,5 +30,83 @@ mod day1 {
                 }
             }
         }
+    }
+}
+mod day2 {
+    struct Rule {
+        min: u32,
+        max: u32,
+        chr: char,
+    }
+    struct Password {
+        rules: Rule,
+        password: String,
+    }
+    impl Password {
+        fn is_valid_count(&self) -> bool {
+            let count = self
+                .password
+                .chars()
+                .filter(|&c| c == self.rules.chr)
+                .count() as u32;
+            self.rules.min <= count && count <= self.rules.max
+        }
+        fn is_valid_position(&self) -> bool {
+            let start_valid = self
+                .password
+                .chars()
+                .nth(self.rules.min as usize - 1)
+                .unwrap()
+                == self.rules.chr;
+            let end_valid = self
+                .password
+                .chars()
+                .nth(self.rules.max as usize - 1)
+                .unwrap()
+                == self.rules.chr;
+            start_valid ^ end_valid
+        }
+    }
+    impl std::str::FromStr for Password {
+        type Err = &'static str;
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let mut s = s.split(' ');
+            let (min, max) = {
+                let mut bounds = s.next().unwrap().split('-').map(|num| num.parse().unwrap());
+                (bounds.next().unwrap(), bounds.next().unwrap())
+            };
+            let chr = s.next().unwrap().chars().next().unwrap();
+            let rules = Rule { min, max, chr };
+            let password = s.next().unwrap().to_string();
+            Ok(Password { rules, password })
+        }
+    }
+    pub fn part1() {
+        let input: Vec<Password> = include_str!("../inputs/day2.txt")
+            .lines()
+            .map(|line| line.parse().unwrap())
+            .collect();
+
+        println!(
+            "{}",
+            input
+                .iter()
+                .filter(|password| password.is_valid_count())
+                .count()
+        )
+    }
+    pub fn part2() {
+        let input: Vec<Password> = include_str!("../inputs/day2.txt")
+            .lines()
+            .map(|line| line.parse().unwrap())
+            .collect();
+
+        println!(
+            "{}",
+            input
+                .iter()
+                .filter(|password| password.is_valid_position())
+                .count()
+        )
     }
 }
